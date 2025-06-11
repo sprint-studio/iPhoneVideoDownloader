1be03c5c9229008fcdfe4ab9186c4f3d39e7ea0e
thekage91 <thekage91@gmail.com>
2025-06-11 21:45:05 +0200

    Initial Commit

---
+++ iPhoneVideoDownloader/iPhoneVideoDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneVideoDownloaderApp.swift
+//  iPhoneVideoDownloader
+//
+//  Created by Davide Sicignani on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneVideoDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iPhoneVideoDownloaderTests/iPhoneVideoDownloaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  iPhoneVideoDownloaderTests.swift
+//  iPhoneVideoDownloaderTests
+//
+//  Created by Davide Sicignani on 11/06/25.
+//
+
+import Testing
+
+struct iPhoneVideoDownloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
